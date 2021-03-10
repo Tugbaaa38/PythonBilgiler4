16e24cb1aa9198e750611a8d7d92ef033eb44e3f
Tugba <77529943+Tugbaaa38@users.noreply.github.com>
2021-03-10 18:53:28 +0300

    Add files via upload

---
+++ python5.py
@@ -0,0 +1,11 @@
+#Kisaca Metotlar
+mesaj="mutlu ol"
+print(mesaj.capitalize()) #capitalize() metodu:Karakter dizinin ilk harfini buyuk harf yapar.
+print(mesaj.title())      #title() metodu:Karakter dizisinin icindeki tum kelimelerin ilk harfini buyuk yapar.
+print(mesaj.upper())      #upper() metodu:Tum harfleri buyuk harfe cevirir.
+print(mesaj.lower())      #lower() metodu:Tum harfleri kucuk harfe cevirir.
+print(mesaj.replace('mutlu','kendin'))  #replace: ilk kelime ile ikinci kelimenin yerini degistirir.
+#ciktisi: kendin ol
+
+cumle="      Yasamak Guzel Sey"
+print(cumle.strip())     #strip():Bastaki bosluklari siler,aralardaki bosluklara dokunmaz.
